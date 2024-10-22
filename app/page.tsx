0540385e60b24f08c1dabaa9b769d165ed7de405
Starosta Gergő Bence <sgergostarosta@gmail.com>
2024-10-22 21:42:56 +0200

    Adding Clerk Authentication to the project

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>Hello world!!!</div>
+    <div>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
   );
 }
