1ec82a87ec7495b3cc1f4cf1333c52bcd448cba2
Starosta Gergő Bence <sgergostarosta@gmail.com>
2024-10-22 21:10:03 +0200

    Setting up Drizzle ORM and Vercel postgres DB

---
+++ lib/db/index.ts
@@ -0,0 +1,9 @@
+import { drizzle } from 'drizzle-orm/vercel-postgres';
+const db = drizzle();
+
+/*
+import { sql } from '@vercel/postgres';
+import { drizzle } from 'drizzle-orm/vercel-postgres';
+
+const db = drizzle({ client: sql })
+*/
